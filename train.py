d9a52dcd4717026cbf7d98a5650075c521abf8f3
PhilippThoelke <p.thoelke@gmx.de>
2019-09-24 17:18:24 +0800

    Add tensorboard logging

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ from environment import UnoEnvironment
 
 PLAYER_COUNT = 4
 COLLECTOR_THREADS = 2
-REPORT_FREQUENCY = 1000
 EPSILON_DECAY = 0.999999
 MIN_EPSILON = 0.01
 
@@ -16,7 +15,6 @@ def run(agent):
     env = UnoEnvironment(PLAYER_COUNT)
 
     counter = 0
-    metrics = {'cumulative_reward': [], 'mean_reward': [], 'episode_length': [], 'epsilon': []}
     while True:
         done = False
         state = None
@@ -43,19 +41,16 @@ def run(agent):
                 # decay epsilon
                 epsilon *= EPSILON_DECAY
                 epsilon = max(epsilon, MIN_EPSILON)
+
+        # log metrics
+        agent.logger.scalar('cumulative_reward', np.sum(rewards))
+        agent.logger.scalar('mean_reward', np.mean(rewards))
+        agent.logger.scalar('game_length', len(rewards))
+        agent.logger.scalar('epsilon', epsilon)
+        agent.logger.scalar('players_left', env.players_left())
+
         # reset the environment for the next episode
         env.reset()
-        metrics['cumulative_reward'].append(np.sum(rewards))
-        metrics['mean_reward'].append(np.mean(rewards))
-        metrics['episode_length'].append(len(rewards))
-        metrics['epsilon'].append(epsilon)
-
-        counter += 1
-        if counter % REPORT_FREQUENCY == 0:
-            print(f'Metrics {counter}')
-            [print(f'{name}: {np.mean(metric)}') for name, metric in metrics.items()]
-            print()
-            metrics = {'cumulative_reward': [], 'mean_reward': [], 'episode_length': [], 'epsilon': []}
 
 if __name__ == '__main__':
     # initialize the training agent
